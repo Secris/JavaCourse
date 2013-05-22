3d345c7655d22df0b506725df4cb1781315392f9
Sean Thames <secris1@gmail.com>
2013-05-21 20:15:32 -0400

    Working through the follow along

---
+++ m1/YouDoIt/DemoArray.java
@@ -0,0 +1,17 @@
+public class DemoArray
+{
+	public static void main(String[] args) 
+	{
+		double[] salary = new double[4];
+		salary[0] = 6.25;
+		salary[1] = 6.55;
+		salary[2] = 10.25;
+		salary[3] = 16.85;
+		
+		System.out.println("Salaries one by one are:");
+		System.out.println(salary[0]);
+		System.out.println(salary[1]);
+		System.out.println(salary[2]);
+		System.out.println(salary[3]);
+	}
+}
---
+++ m1/YouDoIt/DemoArray2.java
@@ -0,0 +1,13 @@
+public class DemoArray2
+{
+	public static void main(String[] args) 
+	{
+		double[] salary = {6.25, 6.55, 10.25, 16.85};
+		
+		System.out.println("Salaries one by one are:");
+		System.out.println(salary[0]);
+		System.out.println(salary[1]);
+		System.out.println(salary[2]);
+		System.out.println(salary[3]);
+	}
+}
