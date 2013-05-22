3947a19125bf5403e6a9223b59fdce7ff243c2cb
Sean Thames <secris1@gmail.com>
2013-05-22 02:06:28 -0400

    Added document information

---
+++ m1/ex15/Quiz.java
@@ -0,0 +1,21 @@
+/*
+	Sean Thames
+	CIST-2372
+	2013-05-22
+	Quiz.java
+	Chapter 8
+	Exercise 15
+	
+	Description:
+	Write an application that contains an array of 10 multiple-choice quiz 
+	questions related to your favorite hobby. Each question contains three 
+	answer choices. Also create an array that holds the correct answer to each
+	question -- A, B, or C. Display each question and verify that the user 
+	enters only A, B, or C as the answer -- if not, keep prompting the user 
+	until a valid response is entered. If the user responds to a question 
+	correctly, display "Correct!"; otherwise, display "The correct answer is" 
+	and the letter of the correct answer. After the user answers all the 
+	questions, display the number of correct and incorrect answers. Save the 
+	file as Quiz.java
+*/
+
