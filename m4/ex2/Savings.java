c281e7236a7bfa1d8e95dc5373627dfcab50f3ea
Sean Thames <secris1@gmail.com>
2013-06-15 06:00:10 -0400

    Added all placeholder files with their respective JavaDOC header comments

---
+++ m4/ex2/Savings.java
@@ -0,0 +1,27 @@
+/**
+ * Programmer:			Sean Thames
+ * Date:				2013-06-14
+ * Filename:			Savings.java
+ * Assignment: 			Ch 11 Ex 2
+ * 
+ * Description:		A) Create an abstract class named "Account" for a bank.
+ * Include an integer field for the account number and a double field 
+ * for the account balance. Also include a constructor that requires an 
+ * account number and that sets the balance to 0.0. Include a set method
+ * for the balance. Also include two abstract get methods -- one for 
+ * each field. Create two child classes of Account: Checking and Savings.
+ * Within the Checking class, the get method displays the String 
+ * "Checking Account Information", the account number, and the balance. 
+ * Within the Savings class, add a field to hold the interest rate, and 
+ * require the Savings constructor to accept an argument for the value 
+ * of the interest rate. The Savings get method displays the String 
+ * "Savings Account Information", the account number, the balance, and 
+ * the interest rate. Write an application that demonstrates you can 
+ * instantiate and display both Checking and Savings objects. Save the 
+ * files as Account.java, Checking.java, Savings.java, and 
+ * DemoAccounts.java
+ * 
+ * B) Write an application name AccountArray in which you enter data for
+ * a mix of 10 Checking and Savings accounts. Use a for loop to display 
+ * the data. Save the file as AccountArray.java
+*/
---
+++ m4/ex8/Insurance.java
@@ -0,0 +1,21 @@
+/**
+ * Programmer:			Sean Thames
+ * Date:				2013-06-14
+ * Filename:			Insurance.java
+ * Assignment: 			Ch 11 Ex 8
+ * 
+ * Description:			Write an application named UseInsurance that uses
+ * an abstract Insurance class and Health and Life subclasses to display
+ * different types of insurance policies and the cost per month. The 
+ * Insurance class contains a String representing the type of insurance
+ * and a double that holds the monthly price. The Insurance class 
+ * constructor requires a String argument indicating the type of 
+ * insurance, but the Life and Health class constructors require no 
+ * arguments. The Insurance class contains a get method for each field;
+ * it also contains two abstract methods named setCost() and display().
+ * The Life class setCost() method sets the monthly fee to $36, and the 
+ * Health class sets the monthly fee to $196. Write an application named
+ * UseInsurance that prompts the user for the type of insurance to be 
+ * displayed, and then create the appropriate object. Save the files as
+ * Life.java, Health.java, Insurance.java, and UseInsurance.java.
+*/
