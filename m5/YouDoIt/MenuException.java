4f886b5ddc626cdc55df9084aa6f5f6375fab0a1
Sean Thames <secris1@gmail.com>
2013-06-22 03:10:13 -0400

    Completed the You Do It sections

---
+++ m5/YouDoIt/MenuException.java
@@ -0,0 +1,7 @@
+public class MenuException extends Exception
+{
+	public MenuException(String choice)
+	{
+		super(choice);
+	}
+}
--- m5/YouDoIt/temp.java
+++
@@ -1,8 +0,0 @@
-/**
- * Programmer:			Sean Thames
- * Date:				2013-06-20
- * Filename:			temp.java
- * Assignment:			Ch 12 Ex 1
- *
- * Description:
-*/
