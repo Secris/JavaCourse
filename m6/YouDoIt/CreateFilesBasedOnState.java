b5da67d0dda2ed0ad5a6696e1c3dfb611a3f693e
Sean Thames <secris1@gmail.com>
2013-06-29 04:40:12 -0400

    Fixed
    Compile error was due to a problem with the example in the book.

--- m6/YouDoIt/CreateFilesBasedOnState.java
+++ m6/YouDoIt/CreateFilesBasedOnState.java
@@ -12,13 +12,24 @@ public class CreateFilesBasedOnState
 	{
 		Scanner input = new Scanner(System.in);
 
-		Path inStateFile = Paths.get("/home/sean/dev/JavaCourse/m6/YouDoIt/InStateCusts.txt");
-		Path outOfStateFile = Paths.get("/home/sean/dev/JavaCourse/m6/YouDoIt/OutOfStateCusts.txt");
+		/*
+			The following lines were modified from the originals in the book
+			for the following reasons:
+			1) Book version fails to compile due to improper Paths.get() call
+			2) Revised version from the book publisher throws IOException: file
+			not found when trying to create the file.
+			3) Cross platform compatability
+		*/
+		Path inStateName = Paths.get("InStateCusts.txt");
+		Path outOfStateName = Paths.get("OutOfStateCusts.txt");
+
+		Path inStateFile = inStateName.toAbsolutePath();
+		Path outOfStateFile = outOfStateName.toAbsolutePath();
 
 		final String ID_FORMAT = "000";
 		final String NAME_FORMAT = "          ";
 		final int NAME_LENGTH = NAME_FORMAT.length();
-		final String HOME_STATE = "WI";
+		final String HOME_STATE = "GA";
 		final String BALANCE_FORMAT = "0000.00";
 		String delimiter = ",";
 		String s = ID_FORMAT + delimiter + NAME_FORMAT + delimiter + HOME_STATE + delimiter + BALANCE_FORMAT + System.getProperty("line.separator");
@@ -35,6 +46,58 @@ public class CreateFilesBasedOnState
 
 		createEmptyFile(inStateFile, s);
 		createEmptyFile(outOfStateFile, s);
+
+		try
+		{
+			fcIn = (FileChannel)Files.newByteChannel(inStateFile, CREATE, WRITE);
+			fcOut = (FileChannel)Files.newByteChannel(outOfStateFile, CREATE, WRITE);
+
+			System.out.print("Enter customer account number >> ");
+			idString = input.nextLine();
+			while(!(idString.equals(QUIT)))
+			{
+				id = Integer.parseInt(idString);
+
+				System.out.print("Enter name for customer >> ");
+				name = input.nextLine();
+				StringBuilder sb = new StringBuilder(name);
+				sb.setLength(NAME_LENGTH);
+				name = sb.toString();
+
+				System.out.print("Enter state >> ");
+				state = input.nextLine();
+
+				System.out.print("Enter balance >> ");
+				balance = input.nextDouble();
+				input.nextLine();
+				DecimalFormat df = new DecimalFormat(BALANCE_FORMAT);
+
+				s = idString + delimiter + name + delimiter + state + delimiter + df.format(balance) + System.getProperty("line.separator");
+				byte data[] = s.getBytes();
+				ByteBuffer buffer = ByteBuffer.wrap(data);
+
+				if(state.equals(HOME_STATE))
+				{
+					fcIn.position(id * RECSIZE);
+					fcIn.write(buffer);
+				}
+				else
+				{
+					fcOut.position(id * RECSIZE);
+					fcOut.write(buffer);
+				}
+
+				System.out.print("Enter next customer account number or " + QUIT + " to quit >> ");
+				idString = input.nextLine();
+			}
+			fcIn.close();
+			fcOut.close();
+		}
+		// Book example is missing catch statement
+		catch(Exception e)
+		{
+			System.out.println("Error: " + e);
+		}
 	}
 
 	public static void createEmptyFile(Path file, String s)
@@ -43,7 +106,7 @@ public class CreateFilesBasedOnState
 
 		try
 		{
-			OutputStream outputStr = new BufferedOutputStream(file.newOutputStream(CREATE));
+			OutputStream outputStr = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
 			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStr));
 
 			for(int count = 0; count < NUMRECS; ++count)
