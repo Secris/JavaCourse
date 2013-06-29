4b72538c2baac6d5456ad7e25291a05ffb1d429b
Sean Thames <secris1@gmail.com>
2013-06-29 03:54:58 -0400

    Added JavaDOC info to placeholder files

---
+++ m6/ex5/temp.java
@@ -0,0 +1,23 @@
+/**
+ * Programmer:			Sean Thames
+ * Date:				2013-06-20
+ * Filename:			temp.java
+ * Assignment:			Ch 13 Ex 5
+ *
+ * Description:			A) Create a program that accepts any number of
+ * first names, last names, and associated phone numbers from the keyboard
+ * and saves the data to a file. Save the program as WritePhoneList.java.
+ * When you execute the program, enter multiple records that have the
+ * same first name because you will search for repeated first names in
+ * part c of this exercise.
+ *
+ * B) Write an application that reads the file created by the
+ * WritePhoneList application and displays the records. Save the file as
+ * DisplaySavedPhoneList.java
+ *
+ * C) Write an application that allows you to enter any first name and
+ * displays all of the last names and phone numbers for records with the
+ * given first name. Display an appropriate message if the first name
+ * cannot be found in the input file. Save the file as
+ * DisplaySelectedNumbers.java
+*/
