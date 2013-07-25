e66e1b7a69308da35e5aa4230f8e9b9ed0febe96
Sean Thames <secris1@gmail.com>
2013-07-25 15:35:48 -0400

    Added JavaDOC

---
+++ m10/ex2/JPasswordA.java
@@ -0,0 +1,19 @@
+/**
+ * Programmer:			Sean Thames
+ * Date:				2013-07-25
+ * Filename:			JPasswordA.java
+ * Assignment:			Ch 17 Ex 2
+ *
+ * Description:         A) Create a JApplet that asks a user to enter a 
+ * password into a JTextField and to then press Enter. Compare the 
+ * password to "Rosebud"; if it matches exactly, display "Access Granted".
+ * If not, display "Access Denied". Save the file as JPasswordA.java.
+ * 
+ * B) Modify the password applet in Exercise 2a to ignore differences in
+ * case between the typed password and "Rosebud". Save the file as 
+ * JPasswordB.java.
+ * 
+ * C) Modify the password applet in Exercise 2b to compare the password 
+ * to a list of five valid passwords: Rosebud, Redrum, Jason, Surrender,
+ * or Dorothy. Save the file as JPasswordC.java
+ */
---
+++ m10/ex8/JCalculatePay.java
@@ -0,0 +1,18 @@
+/**
+ * Programmer:			Sean Thames
+ * Date:				2013-07-25
+ * Filename:			JCalculatePay.java
+ * Assignment:			Ch 17 Ex 8
+ *
+ * Description:         A) Create a payroll JApplet named JCalculatePay 
+ * that allows the user to enter two double values: hours worked and an 
+ * hourly rate. When the user clicks a JButton, gross pay is calculated.
+ * Display the result in a JTextField that appears on screen only after 
+ * the user clicks the JButton. Save the file as JCalculatePay.java.
+ * 
+ * B) Modify the payroll applet created in Exercise 8a so that federal 
+ * withholding tax is subtracted from gross pay based on the values in 
+ * Table 17-1. Display the gross pay, tax amount, and net pay. Save the 
+ * file as JCalculatePay2.java.
+ */
+
